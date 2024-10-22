8666f9efd8652f22dcb28c5073417270e5f5cdc7
maz0uz <124688655+maz0uz@users.noreply.github.com>
2024-10-22 17:20:33 +0300

    Add files via upload

---
+++ Lab 6/csa_8.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/22/2024 04:34:21 PM
+// Design Name: 
+// Module Name: csa_8
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module csa_8(input [0:7] A, [0:7] B, Cin, output S[0:7], Cout);
+
+    rca_4 rca1(.A(A[0:3]),.B(B[0:3]),.Cin(Cin),.S(S[0:3]),.Cout(X
+endmodule
